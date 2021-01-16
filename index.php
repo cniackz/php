be52d48be54f335daa0ce3f15898f03959af0c4a
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-16 16:05:23 -0500

    a

--- index.php
+++ index.php
@@ -288,7 +288,8 @@ while( $row = $result->fetch_assoc()){
     $comentario = $comentario . 'id: ' . $row['id'] . '<BR>';
     $comentario = $comentario . 'fecha: ' . $row['fecha'];
     $comentario = $comentario . '<BR> nombre: ' . $row['nombre'] . '<BR>';
-    $comentario = $comentario . 'comentario:<BR>' . $row['comentario'] . '</p>';
+    $comentario = $comentario . 'comentario:<BR><p class="comentarios"' 
+    $comentario = $comentario . $row['comentario'] . '</p></p>';
     echo $comentario;
     echo '<BR>';
     echo '<hr>';
