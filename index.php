3552128eced08098514363c70573c70b9a1a3a54
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-17 07:56:41 -0500

    a

--- index.php
+++ index.php
@@ -167,7 +167,12 @@ font-size:60px;
 #
 ################################################################################
 -->
-<script src='https://ili4x.github.io/inputEmoji/inputEmoji.js'></script>
+<script
+    src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"
+>
+</script>
 <script type="text/javascript">
 // To reload with the button actualizar
 function refrescar_comentarios() {
