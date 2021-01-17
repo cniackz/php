a2e918160fd36e9486153f29c964700f6c5c55ed
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-17 07:50:23 -0500

    a

--- index.php
+++ index.php
@@ -172,65 +172,8 @@ font-size:60px;
     integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
     crossorigin="anonymous"
 >
-$(function () {
-  $('textarea,input').emoji();
-})
-
-$(function () {
-  $('textarea,input').emoji({
-    button:'&#x1F642;'
-  });
-})
-$(function () {
-  $('textarea,input').emoji({
-    place:'after'
-  });
-})
-$(function () {
-  $('textarea,input').emoji({
-    fontSize:'20px'
-  });
-})
-$(function () {
-
-  $('textarea,input').emoji({
-
-    emojis: ['&#x1F642;','&#x1F641;','&#x1f600;','&#x1f601;','&#x1f602;','&#x1f603;','&#x1f604;','&#x1f605;','&#x1f606;','&#x1f607;','&#x1f608;','&#x1f609;','&#x1f60a;','&#x1f60b;','&#x1f60c;','&#x1f60d;','&#x1f60e;','&#x1f60f;','&#x1f610;','&#x1f611;','&#x1f612;','&#x1f613;','&#x1f614;','&#x1f615;','&#x1f616;','&#x1f617;','&#x1f618;','&#x1f619;','&#x1f61a;','&#x1f61b;','&#x1f61c;','&#x1f61d;','&#x1f61e;','&#x1f61f;','&#x1f620;','&#x1f621;','&#x1f622;','&#x1f623;','&#x1f624;','&#x1f625;','&#x1f626;','&#x1f627;','&#x1f628;','&#x1f629;','&#x1f62a;','&#x1f62b;','&#x1f62c;','&#x1f62d;','&#x1f62e;','&#x1f62f;','&#x1f630;','&#x1f631;','&#x1f632;','&#x1f633;','&#x1f634;','&#x1f635;','&#x1f636;','&#x1f637;','&#x1f638;','&#x1f639;','&#x1f63a;','&#x1f63b;','&#x1f63c;','&#x1f63d;','&#x1f63e;','&#x1f63f;','&#x1f640;','&#x1f643;','&#x1f4a9;','&#x1f644;','&#x2620;','&#x1F44C;','&#x1F44D;','&#x1F44E;','&#x1F648;','&#x1F649;','&#x1F64A;']
-
-  });
-
-})
-$(function () {
-
-  $('textarea,input').emoji({
-
-    listCSS: {
-
-      position:'absolute',
-
-      border:'1px solid gray',
-
-      background-color:'#fff',
-
-      display:'none'
-
-    }
-
-  });
-
-})
-$(function () {
-
-  $('textarea,input').emoji({
-
-    rowSize: 10
-
-  });
-
-})
-
-
 </script>
+<script src='https://ili4x.github.io/inputEmoji/inputEmoji.js'></script>
 <script type="text/javascript">
 // To reload with the button actualizar
 function refrescar_comentarios() {
@@ -385,10 +328,6 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     <INPUT type="submit" value="Enviar" class="input-submit" id="inputSubmit">
 </FORM>
 <HR>
-
-
-<textarea name="emoji" placeholder="type message here..."></textarea>
-<input name="emoji" placeholder="type message here..." />
 
 
 
