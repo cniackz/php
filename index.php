04eaa3ac4b46dead06411dbd5be145609dacdc41
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-17 06:25:27 -0500

    a

--- index.php
+++ index.php
@@ -328,7 +328,6 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     </P>
     <TEXTAREA type="text" name="comentario" class="textarea-comentario" id="textareaComentario">
     </TEXTAREA>
-    <BR>
     <INPUT type="submit" value="Enviar" class="input-submit" id="inputSubmit">
 </FORM>
 <HR>
