2a6c38a2cbd956390e09021ef0b0fbf42cb22aaa
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-17 07:28:13 -0500

    a

--- index.php
+++ index.php
@@ -247,6 +247,14 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     )
 }
 </script>
+<script
+src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+</script>
+<script src='inputEmoji.js'>
+$(function () {
+  $('textarea').emoji();
+})
+</script>
 
 
 
@@ -323,6 +331,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
 <HR>
 
 
+<textarea name="emoji" placeholder="type message here..."></textarea>
 
 
 
