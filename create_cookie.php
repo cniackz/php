4f38e8f40a921c5fd69142aca8a5ca7349505da5
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 13:16:33 -0500

    a

--- create_cookie.php
+++ create_cookie.php
@@ -8,24 +8,24 @@ if (!$connection) {
 }
 
 // From https://www.w3schools.com/php/php_superglobals_post.asp
-if ($_SERVER["REQUEST_METHOD"] == 'POST'){
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 
     // This code is to get the comentario and clave from the main page
-    $usuario     = $_POST['usuario']; 
-    $contrasenia = $_POST['contrasenia'];
+    //$usuario     = $_POST['usuario']; 
+    //$contrasenia = $_POST['contrasenia'];
 
-    if(!empty($usuario)){
-        // Only do this if usuario is not empty
-        // Obten todos los usuarios para sacarles su password
-        $find_password = "SELECT password FROM usuarios WHERE nombre = '";
-        $find_password = $find_password . $usuario . "'";
-        $password = $connection->query($find_password);
-        $cookie_value = NULL;
-        $row = $nombre_contrasena->fetch_assoc())
-        if ($row['password'] == $password) {
-            setcookie("usuario", $password, time() + (86400 * 30), "/", 'cesarcelis.com');
-        }
-    }
+    //if(!empty($usuario)) {
+    //    // Only do this if usuario is not empty
+    //    // Obten todos los usuarios para sacarles su password
+    //    $find_password = "SELECT password FROM usuarios WHERE nombre = '";
+    //    $find_password = $find_password . $usuario . "'";
+    //    $password = $connection->query($find_password);
+    //    $cookie_value = NULL;
+    //    $row = $nombre_contrasena->fetch_assoc())
+    //    if ($row['password'] == $password) {
+    //        setcookie("usuario", $password, time() + (86400 * 30), "/", 'cesarcelis.com');
+    //    }
+    //}
 }
 mysqli_close($connection);
 header('Location: login.php');
