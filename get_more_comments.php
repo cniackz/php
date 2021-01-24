783e2e018c8ac6b5f5cf04c0acda432979a4379d
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 18:54:22 -0500

    a

--- get_more_comments.php
+++ get_more_comments.php
@@ -63,7 +63,7 @@ while( $row = $result->fetch_assoc()){
     echo $comentario;
     echo '<hr>';
 }
-echo '<DIV id="moreComments' . $_GET['number_of_comments'] . '"><BUTTON type="button" class="botones" id="boton_cargar_comentarios" onclick="carga_mas_comentarios_js();">Ver mas comentarios</BUTTON></DIV>';
+echo '<DIV id="moreComments' . $_GET['number_of_comments'] - 10 . '"><BUTTON type="button" class="botones" id="boton_cargar_comentarios" onclick="carga_mas_comentarios_js();">Ver mas comentarios</BUTTON></DIV>';
 //echo '<DIV id="moreComments"></DIV>';
 mysqli_close($connection);
 ?>
