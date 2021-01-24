7c35828b3ef065ec49b7dec036e5943cb0da26d4
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 13:23:59 -0500

    a

--- create_cookie.php
+++ create_cookie.php
@@ -19,6 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         // Obten todos los usuarios para sacarles su password
         $find_password = "SELECT password FROM usuarios WHERE nombre = '";
         $find_password = $find_password . $usuario . "'";
+        echo 'find password: ';
         echo $find_password;
         $password = $connection->query($find_password);
         //$cookie_value = NULL;
