d597ae9e8421f6db9522a9ea6e612d4ca0d1e841
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 13:13:41 -0500

    a

--- login.php
+++ login.php
@@ -2,12 +2,12 @@
 
 <?php
 
-    // Este codigo es para mostrar la clave solo si se necesita
+    // Para pedir al usuario que se logie sino hay cookie
     if(!isset($_COOKIE['usuario'])) {
         echo '<P id="usuario">Usuario:</P>';
         echo '<INPUT type="text" name"usuario" class="to_be_defined">';
         echo '<P id="clave_p">Constraseña:</P>';
-        echo '<INPUT type="text" name="clave" class="input-text-clave" id="inputTextClave" >';
+        echo '<INPUT type="text" name="contrasenia" class="input-text-clave" id="inputTextClave" >';
         echo '<BR>';
         echo '<BR>';
     } else {
