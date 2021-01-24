4268dd29e0fecc5bf44d3b243013e61bdf5c1622
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 13:14:32 -0500

    a

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     // Para pedir al usuario que se logie sino hay cookie
     if(!isset($_COOKIE['usuario'])) {
         echo '<P id="usuario">Usuario:</P>';
-        echo '<INPUT type="text" name"usuario" class="to_be_defined">';
+        echo '<INPUT type="text" name="usuario" class="to_be_defined">';
         echo '<P id="clave_p">Constraseña:</P>';
         echo '<INPUT type="text" name="contrasenia" class="input-text-clave" id="inputTextClave" >';
         echo '<BR>';
