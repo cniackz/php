0e80f4295562508871de878e20553147e3e9b6a1
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 14:06:44 -0500

    a

--- delete_cookie.php
+++ delete_cookie.php
@@ -1,6 +1,8 @@
 <?php
 
-        
-            setcookie("usuario", "hola", time() + (86400 * 30), "/", 'cesarcelis.com');
+// This code will delete the cookie by setting an invalid password in the value
+// when hola is set as the user password, then the cookie becomes invalid
+// and hence the user cannot see comments and has to login again
+setcookie("usuario", "hola", time() + (86400 * 30), "/", 'cesarcelis.com');
 
 ?>
