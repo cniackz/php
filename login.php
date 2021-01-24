7b28b3cd5688ba5da3c7639e078b14b0c2a2fe55
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-24 12:56:17 -0500

    a

---
+++ login.php
@@ -0,0 +1,16 @@
+<FORM method="post" action="create_cookie.php">
+
+<?php
+
+    // Este codigo es para mostrar la clave solo si se necesita
+    if(!isset($_COOKIE['usuario'])) {
+        echo '<P id="usuario"Usuario:</P>';
+        echo '<INPUT type="text" name"usuario" class="to_be_defined">';
+        echo '<P id="clave_p">Clave:</P>';    
+        echo '<INPUT type="text" name="clave" class="input-text-clave" id="inputTextClave" >'; 
+    } else {
+        // display chat
+    }
+?>
+    <INPUT type="submit" value="Enviar" class="input-submit" id="inputSubmit">
+</FORM>
