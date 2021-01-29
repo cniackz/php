bb059baa1f1d62aac73a7acd160982ccb5022b1a
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-28 21:37:08 -0500

    a

--- respaldar.php
+++ respaldar.php
@@ -1,52 +1,43 @@
 <?php
 
-$connection = mysqli_connect('localhost', 'root', '', 'cesar');
-if (!$connection) {
- echo "Error: Unable to connect to MySQL." . PHP_EOL;
- echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
- echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
- exit;
+$db_record = 'comentarios';
+$csv_filename = 'db_export_'.$db_record.'_'.date('Y-m-d').'.csv';
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$database = "cesar";
+$conn = mysqli_connect($hostname, $user, $password, $database);
+if (mysqli_connect_errno()) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
-$select = "SELECT * FROM comentarios";
+// create empty variable to be filled with export data
+$csv_export = '';
 
-$export = $connection->query( $select );
+// query to get data from database
+$query = mysqli_query($conn, "SELECT * FROM ".$db_record);
+$field = mysqli_field_count($conn);
 
-$fields = $export->columnCount();
-
-for ( $i = 0; $i < $fields; $i++ )
-{
-    $header .= $export->getColumnMeta( $i ) . "\t";
+// create line with field names
+for($i = 0; $i < $field; $i++) {
+    $csv_export.= mysqli_fetch_field_direct($query, $i)->name.';';
 }
 
-while( $row = $export->fetch_assoc() )
-{
-    $line = '';
-    foreach( $row as $value )
-    {                                            
-        if ( ( !isset( $value ) ) || ( $value == "" ) )
-        {
-            $value = "\t";
-        }
-        else
-        {
-            $value = str_replace( '"' , '""' , $value );
-            $value = '"' . $value . '"' . "\t";
-        }
-        $line .= $value;
+// newline (seems to work both on Linux & Windows servers)
+$csv_export.= '
+';
+
+// loop through database query and fill export variable
+while($row = mysqli_fetch_array($query)) {
+    // create line with field values
+    for($i = 0; $i < $field; $i++) {
+        $csv_export.= '"'.$row[mysqli_fetch_field_direct($query, $i)->name].'";';
     }
-    $data .= trim( $line ) . "\n";
-}
-$data = str_replace( "\r" , "" , $data );
-
-if ( $data == "" )
-{
-    $data = "\n(0) Records Found!\n";                        
+    $csv_export.= '
+';
 }
 
-header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=your_desired_name.xls");
-header("Pragma: no-cache");
-header("Expires: 0");
-print "$header\n$data";
-?>
+// Export the data and prompt a csv file for download
+header("Content-type: text/x-csv");
+header("Content-Disposition: attachment; filename=".$csv_filename."");
+echo($csv_export);
