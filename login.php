5bd836b6279b3f63c04a8c20c7ca7f09de3b2a6b
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-30 22:54:44 -0500

    a

--- login.php
+++ login.php
@@ -7,13 +7,23 @@
     // Para pedir al usuario que se logie sino hay cookie
     if(!isset($_COOKIE['usuario'])) {
         // no hay cookie debe logearse
+
     } else {
         if($_COOKIE['usuario'] == 'hola'){
             // Cuando el valor de la cookie es hola, significa que el valor es
             // invalido, entonces el usuario debe logearse de nuevo
         } else {
-            // display chat (send to index.php where we display it)
-            header('Location: index.php');
+            
+            // Si hay cookie si es la correcta mandalo a index.php donde esta el chat de la familia
+            $passwords = array("cch1987", "ejch1994", "jacl1960", "mahp1965", "larh1989");
+            for ($x = 0; $x < 5; $x++) {
+                if($_COOKIE['usuario'] == $passwords[$x]){
+                    header('Location: index.php');
+                }
+            }
+
+            // Sino es la correcta de la familia, mandalo al general
+            header('Location: general.php');
         }
     }
     echo '<P id="usuario">Usuario:</P>';
--- mp3.php
+++ mp3.php
@@ -1,6 +1,8 @@
 <?php
+//https://coursesweb.net/php-mysql/output-force-download-mp3-php#:~:text=Force%20Download%20MP3%20file%20with%20PHP&text=header('Content%2DDisposition%3A%20attachment,%22attachment%22%20to%20force%20download.
+// Force Download MP3 file with PHP
 
-$mp3 ='abc.mp3';
+$mp3 ='abc.mp3'; // path to mp3, so here
 
 if(file_exists($mp3)) {
   header('Content-Type: audio/mpeg');
