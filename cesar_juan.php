30a898cba9e43acff2a8e343f9d7b12441270ae5
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-30 20:56:12 -0500

    a

--- cesar_juan.php
+++ cesar_juan.php
@@ -27,6 +27,8 @@ $sql = 'SELECT * FROM cesar_juan ORDER BY id DESC';
 $result = $connection->query($sql);
 
 echo '
+<head></head>
+<meta content='width=device-width, initial-scale=1' name='viewport'/>
 <A HREF="index.php">Volver al grupo de la familia</A>
 </BR>
 </BR>
