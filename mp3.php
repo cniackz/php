c394fdccf39e43cc14e3a0e30c28f937428d7149
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-01-30 22:34:38 -0500

    a

--- mp3.php
+++ mp3.php
@@ -1,6 +1,18 @@
 <?php
-header("Content-type: application/octet-stream"); 
-header("Content-Disposition: attachment; filename=abc.mp3"); 
-echo $output;
-die();
+
+$mp3 ='abc.mp3';
+
+if(file_exists($mp3)) {
+  header('Content-Type: audio/mpeg');
+  header('Content-Disposition: attachment; filename="mp3_file.mp3"');
+  header('Content-length: '. filesize($mp3));
+  header('Cache-Control: no-cache');
+  header('Content-Transfer-Encoding: chunked'); 
+  readfile($mp3);
+  exit;
+}
+
 ?>
+
+
+
