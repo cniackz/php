862ad9a1bc76caac76696c6dba68995db73b6023
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-02-04 19:20:52 -0500

    a

--- index.php
+++ index.php
@@ -478,6 +478,7 @@ function tamano_de_letra() {
   var tamano = prompt("Por favor ponga el tamano de letra", "20px");
   if (person != null) {
     fontSize = tamano;
+    setea_estilo_para_pc();
   }
 }
 
