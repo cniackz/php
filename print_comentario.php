2d380fc3ca9270357476144b98a56ece44ed5cbe
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-02-06 12:20:22 -0500

    a

--- print_comentario.php
+++ print_comentario.php
@@ -17,7 +17,7 @@ $parrafo = "
 
 <p class=\"label_de_$nombre\">
     $nombre 🕙 $fecha $device<BR>
-    <button id=\"$id\" ondblclick=\"funcion_alerta($id, '$nombre');\" class=\"comentario_de_$nombre\">
+    <button id=\"$id\" ondblclick=\"funcion_alerta($id, '$nombre');\" class=\"comentarios_de_$nombre\">
         $comentario
     </button>
 </p>
