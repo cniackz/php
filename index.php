1ae42286edbf0c866b7398b3585f2ce832fdde31
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-02-06 07:59:51 -0500

    a

--- index.php
+++ index.php
@@ -705,7 +705,7 @@ $contador = 0;
 echo '<DIV id="moreNewComments"></DIV>';
 
 $where_clause = '' // For now is empty, later we need to consider id 50, id bigger than, id betwee a and b...
-$result = get_comments($connection, $where_clause, $number_of_hour)
+$result = get_comments($connection, $where_clause, $number_of_hour);
 
 while( $row = $result->fetch_assoc()){
     $comentario = '<p class="label_de_' . $row['nombre'] . '">';
