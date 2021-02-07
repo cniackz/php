72d3e3488a9f2bc07e628ba92d999121df2741a9
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-02-07 09:01:16 -0500

    a

--- cesar_martha.php
+++ cesar_martha.php
@@ -12,11 +12,11 @@ if (!$connection) {
 
 $numeroDeHoras = 0;
 $puede_continuar = FALSE;
-if($_COOKIE['usuario']=='cch1987'){
+if(get_username($_COOKIE['usuario'])=='cesar'){
 	$puede_continuar = TRUE;
 	$numeroDeHoras = 5;
 }
-if($_COOKIE['usuario']=='mahp1965'){
+if(get_username($_COOKIE['usuario'])=='martha'){
 	$puede_continuar = TRUE;
 	$numeroDeHoras = 6;
 }
