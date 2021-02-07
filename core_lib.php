37f63e8d826d411f669a4380ec2dff941dab25d7
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-02-07 08:16:13 -0500

    a

--- core_lib.php
+++ core_lib.php
@@ -1,8 +1,27 @@
 <?
 
-function check_password($connection, $password){
+function get_db_connectio(){
+
+	// To get db connection
+	$connection = mysqli_connect('localhost', 'root', '', 'cesar');
+	if (!$connection) {
+	 echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	 echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	 echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	 exit;
+	}
+
+	return $connection;
+
+}
+
+function check_password($password){
 	// Return TRUE if password exist
 	// Return FALSE if password does not exist
+
+	$connection = get_db_connectio();
+
+	// Main logic
 	$final_result = FALSE;
 	$sql = 'SELECT password FROM usuarios WHERE password = $password';
 	$result = $connection->query($sql);
@@ -12,6 +31,9 @@ function check_password($connection, $password){
 	} else {
 		$final_result = FALSE;
 	}
+
+	mysqli_close($connection);
+
 	return $final_result;
 }
 
