5b4f064e7be29fa28e80690bf9a2d10f549086b1
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-10-16 10:07:09 -0400

    a

--- pagos.php
+++ pagos.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<style>
+			header { border-bottom: 1px solid #000; }
+			header > h1 { display: inline-block; }
+			header span { margin-left: 100px; }
+		</style>
+	</head>
 	<body>
 
 		<BUTTON onclick="window.location.replace('https://cesarcelis.com/index.php');">
             Go Back
         </BUTTON>
 
-		<h1>Pagos</h1>
-		<h2>Benji</h2>
-		<p>30</p>
+        <header>
+			<h1>Benji</h1>
+			<p>$30 CAD</p>
+		</header>
 
 	</body>
 </html>
