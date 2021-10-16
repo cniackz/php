d68f3131cbf9f72550e3cae7697254058afcbbfb
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-10-16 10:35:10 -0400

    a

--- pagos.php
+++ pagos.php
@@ -26,79 +26,79 @@
         </BUTTON>
 
         <header id='top_header'>
-            <h1>WiFi</h1>
+            <h1>01. WiFi</h1>
             <span> ---> </span>
             <span>$20.00 CAD</span>
         </header>
 
         <header>
-            <h1>Benji</h1>
+            <h1>02. Benji</h1>
             <span> ---> </span>
             <span>$30.00 CAD</span>
         </header>
 
         <header>
-            <h1>LCBO</h1>
+            <h1>03. LCBO</h1>
             <span> ---> </span>
             <span>$25.00 CAD</span>
         </header>
 
         <header>
-            <h1>Mobile</h1>
+            <h1>04. Mobile</h1>
             <span> ---> </span>
             <span>$50.85 CAD</span>
         </header>
 
         <header>
-            <h1>Renta</h1>
+            <h1>05. Renta</h1>
             <span> ---> </span>
             <span>$525.00 CAD</span>
         </header>
 
         <header>
-            <h1>Gasolina</h1>
+            <h1>06. Gasolina</h1>
             <span> ---> </span>
             <span>$50.00 CAD</span>
         </header>
 
         <header>
-            <h1>Parents</h1>
+            <h1>07. Parents</h1>
             <span> ---> </span>
             <span>$200.00 CAD</span>
         </header>
 
         <header>
-            <h1>Laundry</h1>
+            <h1>08. Laundry</h1>
             <span> ---> </span>
             <span>$15.00 CAD</span>
         </header>
 
         <header>
-            <h1>Car Loan</h1>
+            <h1>09. Car Loan</h1>
             <span> ---> </span>
             <span>$130.00 CAD</span>
         </header>
 
         <header>
-            <h1>Groceries</h1>
+            <h1>10. Groceries</h1>
             <span> ---> </span>
             <span>$190.00 CAD</span>
         </header>
 
         <header>
-            <h1>XYZ Storage</h1>
+            <h1>11. XYZ Storage</h1>
             <span> ---> </span>
             <span>$67.79 CAD</span>
         </header>
 
         <header>
-            <h1>Restaurants</h1>
+            <h1>12. Restaurants</h1>
             <span> ---> </span>
             <span>$100.00 CAD</span>
         </header>
 
         <header>
-            <h1>Car Insurance</h1>
+            <h1>13. Car Insurance</h1>
             <span> ---> </span>
             <span>$70.00 CAD</span>
         </header>
