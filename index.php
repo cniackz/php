e8c7b7067b617a1eef2eb9223265878f72949508
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-10-16 10:00:59 -0400

    a

--- index.php
+++ index.php
@@ -106,15 +106,15 @@ if(get_username($_COOKIE['usuario'])=='jacqui'){
             </BUTTON>
         </TH>';
 }
-echo '<TH>
-    <BUTTON class="botones" onclick="window.location.replace(\'https://cesarcelis.com/pagos.php\');>
-        Pagos
-    </BUTTON>
-</TH>'
 ?>
         <TH>
             <BUTTON type="button" class="botones" id="boton_de_logout" onclick="logout();">
                 Salir
+            </BUTTON>
+        </TH>
+        <TH>
+            <BUTTON class="botones" onclick="window.location.replace('https://cesarcelis.com/pagos.php');">
+                Pagos
             </BUTTON>
         </TH>
     </TR>
