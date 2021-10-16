8e3ac6c4ec22540f654165155643dccc45c76c4f
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-10-16 10:11:16 -0400

    a

--- pagos.php
+++ pagos.php
@@ -5,7 +5,7 @@
 			header { border-bottom: 1px solid #000; }
 			header > h1 { display: inline-block; }
 			header span {
-				margin-left: 100px;
+				margin-left: 50px;
 				font-size: 40px;
 			}
 		</style>
