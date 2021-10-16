41f95764ec3abe661480e5e7123274f18961519e
Cesar Celis <celis.hernandez.cesar@gmail.com>
2021-10-16 10:15:01 -0400

    a

--- pagos.php
+++ pagos.php
@@ -1,32 +1,38 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<style>
-			header {
-				border-top: 1px solid #000;
-				border-bottom: 1px solid #000;
-			}
-			header > h1 { display: inline-block; }
-			header span {
-				margin-left: 10px;
-				font-size: 40px;
-			}
-			BUTTON {
-				margin-bottom: 10px;
-			}
-		</style>
-	</head>
-	<body>
+    <head>
+        <style>
+            header {
+                border-top: 1px solid #000;
+                border-bottom: 1px solid #000;
+            }
+            header > h1 { display: inline-block; }
+            header span {
+                margin-left: 10px;
+                font-size: 40px;
+            }
+            BUTTON {
+                margin-bottom: 10px;
+            }
+        </style>
+    </head>
+    <body>
 
-		<BUTTON onclick="window.location.replace('https://cesarcelis.com/index.php');">
+        <BUTTON onclick="window.location.replace('https://cesarcelis.com/index.php');">
             Go Back
         </BUTTON>
 
         <header>
-			<h1>Benji</h1>
-			<span> ---> </span>
-			<span>$30 CAD</span>
-		</header>
+            <h1>Benji</h1>
+            <span> ---> </span>
+            <span>$30 CAD</span>
+        </header>
 
-	</body>
+        <header>
+            <h1>Gasolina</h1>
+            <span> ---> </span>
+            <span>$50 CAD</span>
+        </header>
+
+    </body>
 </html>
