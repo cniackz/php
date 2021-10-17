b2e7ff56fddc4eb327c496c3c9c40ab7ec3180ac
cesar <celis.hernandez.cesar@gmail.com>
2021-10-16 20:45:36 -0400

    a

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ require 'core_lib.php';
 <body id="body">
 <meta content='width=device-width, initial-scale=1' name='viewport'/>
 
-<iframe src="https://www.cesarcelis.com/clock.php" height="400" width="420" title="time"></iframe>
+<iframe src="https://www.cesarcelis.com/clock.php" height="410" width="420" title="time"></iframe>
 
 <TABLE id="topid">
     <TR>
